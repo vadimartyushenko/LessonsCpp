fddc6850b1305ed0c6710befa0df52614adb6526
Vadim Artyushenko <artushenkomail@mail.ru>
2023-03-13 23:10:52 +0700

    added method for matrix create

--- Lessons/Lessons.cpp
+++ Lessons/Lessons.cpp
@@ -9,26 +9,41 @@
 using namespace std;
 using namespace std::placeholders;
 
-bool isMaxEqual(int x, int max)
+bool isMaxEqual(int x, int max, int min)
 {
-    return x == max;
+    return x <= max && x >= min;
 }
 
 int rnd()
 {
     return rand() % 100;
 }
+ void CreateMatrix(vector<vector<int>>& in, int N, int M)
+{
+	for (int i = 0; i < N; ++i)
+	{
+        vector<int> str(M);
+        generate(str.begin(), str.end(), rnd);
+        in.push_back(str);
+	}
+}
 
-int main()
+void main()
 {
     setlocale(LC_ALL, "ru");
     srand(static_cast<unsigned>(time(nullptr)));
 
     vector<int> i_vec(10);
+    vector<vector<int>> mat;
+
+    CreateMatrix(mat, 3, 5);
+    PrintMatrix(mat);
+
     /*for (int i = 0; i < 10; ++i) {
     	//i_vec.push_back(rand() % 100);
         i_vec.push_back(i);
     }*/
+
     generate(i_vec.begin(), i_vec.end(), rnd);
 
 	i_vec.insert(i_vec.begin() + 3, 9);
@@ -40,18 +55,24 @@ int main()
 
     cout << "Min = " << *min_iter << endl;
     cout << "Max = " << *max_iter << endl;
-    auto max = *max_iter;
-    //const auto removed = remove_if(i_vec.begin(), i_vec.end(), [max](const int x) { return x == max;});
-    const auto removed = remove_if(i_vec.begin(), i_vec.end(),bind(isMaxEqual, _1, max));
+
+    //min max range sum
+	/*if (min_iter < max_iter) {
+        cout << "Min before Max" << endl;
+        return;
+    }
+
+    auto sum = accumulate(max_iter + 1, min_iter, 0);
+    */
+
+	//const auto removed = remove_if(i_vec.begin(), i_vec.end(), [max](const int x) { return x == max;});
+    /*const auto removed = remove_if(i_vec.begin(), i_vec.end(), bind(isMaxEqual, _1, 50, 20));
 	i_vec.erase(removed, i_vec.end());
 
     cout << "After deleting: " << endl;
-    for (const int el : i_vec) {
-        cout << el << endl;
-    }
-
+    PrintVector(i_vec);
+    */
     cout << "Size: " << i_vec.size() << endl;
     cout << "Cap: " << i_vec.capacity() << endl;
-    i_vec.shrink_to_fit();
 }
 
